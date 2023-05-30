138e7e3318eb76292abd9ce2167781d95367596f
Landon Delaney <delaneyboys16@gmail.com>
2023-05-30 10:53:29 -0600

    expanding our tests

--- test/login.test.js
+++ test/login.test.js
@@ -9,18 +9,76 @@ it("Should get a login token",async ()=>{
             "Content-Type":"application/json"
         },
         body:JSON.stringify({
-            "userName":"scmurdock@gmail.com",
-            "password":"P@ssw0rd"
+            "userName":"derp@q.com",
+    
+            "password":"Riverside#736"
         })
     }
  
     const response = await fetch('https://dev.stedi.me/login',options);
  
     token = await response.text();
-    console.log("Token "+token);
     const status = response.status;
+    console.log("Token "+token);
+    
  
-    expect(status).toBe(200);
+    
     expect(token.length).toBe(36);
+    expect(status).toBe(200);
+    
+})
+
+it("Should detect a bad password",async ()=>{
  
+    let token = "";//this is an emptry string
+    const options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "userName":"Derp@q.com",
+    
+            "password":"Herp"
+        })
+    }
+ 
+    const response = await fetch('https://dev.stedi.me/login',options);
+ 
+    token = await response.text();
+    const status = response.status;
+    console.log("Token "+token);
+    
+ 
+    
+    expect(token.length).toBe(60);
+    expect(status).toBe(500);
+    
+})
+
+it("Should detect a bad username",async ()=>{
+ 
+    let token = "";//this is an emptry string
+    const options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "userName":"jerp@Baduser.com",
+    
+            "password":"Riverside#736"
+        })
+    }
+ 
+    const response = await fetch('https://dev.stedi.me/login',options);
+ 
+    token = await response.text();
+    const status = response.status;
+    console.log("Token "+token);
+    
+ 
+    expect(token.length).toBe(60);
+    expect(status).toBe(500);
+    
 })
